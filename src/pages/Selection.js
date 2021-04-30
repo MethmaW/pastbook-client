6f23e5dfc3c158b425a09890bece6b85c4b95c01
MethmaW <methma.cw@gmail.com>
2021-04-30 14:36:28 +0530

    Add pages folder - initial structure

---
+++ src/pages/Selection.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Selection = () => {
+	function allowDrop(ev) {
+		ev.preventDefault();
+	}
+
+	function drag(ev) {
+		ev.dataTransfer.setData("text", ev.target.id);
+	}
+
+	function drop(ev) {
+		ev.preventDefault();
+		var data = ev.dataTransfer.getData("text");
+		ev.target.appendChild(document.getElementById(data));
+		console.log(ev.target.id);
+	}
+	return (
+		<>
+			<div id='div1' onDrop={(e) => drop(e)} onDragOver={(e) => allowDrop(e)}>
+				<img
+					src='img_w3slogo.gif'
+					draggable='true'
+					onDragStart={(e) => drag(e)}
+					id='drag1'
+					width='88'
+					height='31'
+				/>
+			</div>
+
+			<div
+				id='div2'
+				onDrop={(e) => drop(e)}
+				onDragOver={(e) => allowDrop(e)}
+			></div>
+
+			<div
+				id='div3'
+				onDrop={(e) => drop(e)}
+				onDragOver={(e) => allowDrop(e)}
+			></div>
+
+			<div
+				id='div4'
+				onDrop={(e) => drop(e)}
+				onDragOver={(e) => allowDrop(e)}
+			></div>
+		</>
+	);
+};
+
+export default Selection;
